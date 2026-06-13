6ac89477ddec1c2eb3a04c59a86292a629fd894c
joonhe-dev <joonhe.dev@gmail.com>
2026-06-13 09:46:33 +0800

    feat: update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 import { Command } from 'commander';
-import { version } from '../package.json' with { type: 'json' };
 
 const program = new Command();
 
 program
   .name('devhammer')
   .description('🔨 A local-first CLI toolset for TypeScript full-stack developers')
-  .version(version);
+  .version('0.1.0');
 
-// Module registration will be added as modules are implemented
-// import { registerConfigCommand } from './commands/config.js';
-// import { registerApiCommand } from './commands/api.js';
-// import { registerScaffoldCommand } from './commands/scaffold.js';
-// import { registerProfileCommand } from './commands/profile.js';
-// import { registerEnvCommand } from './commands/env.js';
+import { registerConfigCommand } from './commands/config.js';
+import { registerApiCommand } from './commands/api.js';
+import { registerScaffoldCommand } from './commands/scaffold.js';
+import { registerProfileCommand } from './commands/profile.js';
+import { registerEnvCommand } from './commands/env.js';
 
-// registerConfigCommand(program);
-// registerApiCommand(program);
-// registerScaffoldCommand(program);
-// registerProfileCommand(program);
-// registerEnvCommand(program);
+registerConfigCommand(program);
+registerApiCommand(program);
+registerScaffoldCommand(program);
+registerProfileCommand(program);
+registerEnvCommand(program);
 
 program.parse();
