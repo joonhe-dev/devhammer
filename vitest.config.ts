37ead92473d3f05a386213d6bed7743c8e4dc978
joonhe-dev <joonhe.dev@gmail.com>
2026-06-13 09:47:29 +0800

    feat: update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'node:path';
 
 export default defineConfig({
+  resolve: {
+    extensionAlias: {
+      '.js': ['.ts', '.js'],
+    },
+  },
   test: {
     globals: true,
     environment: 'node',
